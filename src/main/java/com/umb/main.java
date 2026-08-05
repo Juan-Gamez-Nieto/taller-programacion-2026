906c3761b8435f562daed49bf7bddfc421d91703
Juan-Gamez-Nieto <juangamez.mn@academia.umb.edu.co>
2026-08-04 22:50:49 -0500

    feat: initial project structure with Maven layout

---
+++ src/main/java/com/umb/main.java
@@ -0,0 +1,7 @@
+package com.umb.taller;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Proyecto Taller Programación UMB 2026 cargado correctamente.");
+    }
+}
